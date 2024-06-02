51dc85661a0307935c19ef1ddf96c3ed2956b8ec
MUHAMMAD AUS HIJRI <aushijree@gmail.com>
2024-06-03 01:53:15 +0900

    Add more wallets and read write features. Using Viem

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import { ethers } from 'ethers';
 import Navigation from './Navigation';
+import { CoinbaseWalletSDK } from '@coinbase/wallet-sdk'
+import Select from 'react-select';
+
+import { getAbiItem } from 'viem'
+import { getContract } from 'viem'
+import { createPublicClient, createWalletClient, http, custom } from 'viem'
+
+
+const viemChains = require('viem/chains');
+const options = Object.keys(viemChains).map(chain => ({ value: chain, label: chain }));
+
+ 
 
 function App() {
   const [account, setAccount] = useState(null);
@@ -10,65 +22,43 @@ function App() {
   const [contract, setContract] = useState(null);
   const [abi, setAbi] = useState(null);
   const [values, setValues] = useState(['', '', '', '', '', '', '', '', '']);
-  const [network, setNetwork] = useState('');
+  const [network, setNetwork] = useState('mainnet');
+  const [isAddressTooShort, setIsAddressTooShort] = useState(false);
+  const [isAddressEmpty, setIsAddressEmpty] = useState(false);
+  const [address, setAddress] = useState('');
+  const [output, setOutput] = useState(null);
+
+  console.log(`Network: ${network}`);
+
+
+  const publicClient = createPublicClient({
+    chain: viemChains[network],
+    transport: custom(window.ethereum)
+  })
+
+  const walletClient = createWalletClient({
+    chain: viemChains[network],
+    transport: custom(window.ethereum)
+  })
+   
+
+  const sdk = new CoinbaseWalletSDK({
+    appName: 'ShortcutSC',
+    appChainIds: [1,	42161, 421614]
+  });
 
   const inputValuesRef = useRef({});
 
-  const handleNetworkChange = async (event) => {
-    const network = event.target.value;
+  const handleNetworkChange = async (network) => {
     setNetwork(network);
   
-    const networks = {
-      mainnet: { chainId: '0x1', rpcUrls: ['https://eth.llamarpc.com'] },
-      arbitrum: { 
-        chainId: '0xa4b1', 
-        rpcUrls: ['https://arbitrum.llamarpc.com'],
-        chainName: 'Arbitrum',
-        nativeCurrency: {
-          name: 'ETH',
-          symbol: 'ETH',
-          decimals: 18
-        },
-        blockExplorerUrls: ['https://arbiscan.io/'] 
-      },
-      arbitrum_sepolia: { 
-        chainId: '0x66eee', 
-        rpcUrls: ['https://sepolia-rollup.arbitrum.io/rpc'],
-        chainName: 'Arbitrum Sepolia Testnet',
-        nativeCurrency: {
-          name: 'ETH',
-          symbol: 'ETH',
-          decimals: 18
-        },
-        blockExplorerUrls: ['https://sepolia.arbiscan.io/'] 
-      },
-      // Add more networks here
-    };
-  
-    const networkData = networks[network];
-  
     try {
-      await window.ethereum.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{ chainId: networkData.chainId }],
-      });
+      await walletClient.switchChain({ id: viemChains[network].id });
     } catch (switchError) {
       // This error code indicates that the chain has not been added to MetaMask.
       if (switchError.code === 4902) {
         try {
-          await window.ethereum.request({
-            method: 'wallet_addEthereumChain',
-            params: [
-              {
-                chainId: networkData.chainId,
-                chainName: networkData.chainName,
-                rpcUrls: networkData.rpcUrls,
-                iconUrls: networkData.iconUrls,
-                nativeCurrency: networkData.nativeCurrency,
-                blockExplorerUrls: networkData.blockExplorerUrls,
-              },
-            ],
-          });
+          await walletClient.addChain({ chain: viemChains[network] });
         } catch (addError) {
           // Handle "add" error.
         }
@@ -89,12 +79,16 @@ function App() {
   }, [values]);
 
   const handleConnect = async () => {
+    if (!address) {
+      setIsAddressEmpty(true);
+      return;
+    }
     const abiJson = document.getElementById('contractABI').value;
     const abi = JSON.parse(abiJson);
     setAbi(abi);
-
-    const contractAddress = document.getElementById('contractAddress').value;
-
+  
+    console.log(`Contract ABI:`, abi);
+  
     if (window.ethereum) {
       try {
         // Request account access
@@ -102,61 +96,99 @@ function App() {
         const account = ethers.utils.getAddress(accounts[0]);
         setAccount(account);
         console.log(`Connected with address: ${account}`);
-
+  
         // Initialize the contract
-        const contract = new ethers.Contract(contractAddress, abi, signer);
-        setContract(contract);
+        const contract = getContract({
+          address: address,
+          abi: abi,
+          client: { public: publicClient, wallet: walletClient }
+        });
+  
+        // Call each function that doesn't require inputs and is a view function
+        for (const item of abi) {
+          if (item.type === 'function' && item.stateMutability === 'view' && (item.inputs === undefined || item.inputs.length === 0)) {
+            const data = await publicClient.readContract({
+              address: address,
+              abi: abi,
+              functionName: item.name,
+            });
+            setOutput(prevOutput => ({
+              ...prevOutput,
+              [item.name]: data
+            }));
+          }
+        }
       } catch (error) {
-        console.error("User denied account access");
+        console.error(`Failed to connect: ${error}`);
       }
-    } else {
-      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
   };
 
 
   const handleSign = async (methodName, params) => {
+    
     console.log(`methodName: ${methodName}, params:`, params);
     if (!window.ethereum) {
       return;
     }
   
     try {
+
+      try {
+        await walletClient.switchChain({ id: viemChains[network].id });
+      } catch (switchError) {
+        // This error code indicates that the chain has not been added to MetaMask.
+        if (switchError.code === 4902) {
+          try {
+            await walletClient.addChain({ chain: viemChains[network] });
+          } catch (addError) {
+            // Handle "add" error.
+            console.error(`Failed to add network:`, addError);
+          }
+        }
+      }
+      
       // Find the function object from the contract using the function name
-      const functionObject = contract.interface.getFunction(methodName);
-      console.log('Contract ABI:', contract.interface.fragments);
-console.log('Function object:', functionObject);
+      const functionObject = getAbiItem({ abi, name: methodName });
+    
+      console.log('Contract ABI:', abi);
+      console.log('Function object:', functionObject);
+    
       if (!functionObject) {
         throw new Error(`Function ${methodName} not found on contract`);
       }
   
       // Filter out empty strings from params
       const filteredParams = params.filter(param => param !== '');
+
+      console.log(address)
   
       // Prepare the transaction
-const transaction = {
-  to: contract.address,
-  from: account,
-  data: contract.interface.encodeFunctionData(methodName, filteredParams)
-};
+      const { request } = await publicClient.simulateContract({
+        address: address,
+        abi: abi,
+        functionName: methodName,
+        args: filteredParams,
+        account
+      });
+
+      console.log('Request:', request);
   
       // Estimate the gas for the transaction
-      const gasEstimate = await window.ethereum.request({
-        method: 'eth_estimateGas',
-        params: [transaction]
-      });
+      const gasEstimate = await publicClient.estimateContractGas(request);
+
+      console.log('Gas estimate:', gasEstimate);
   
       // Prepare the transaction for eth_sendTransaction
       const sendTransaction = {
-        ...transaction,
+        ...request,
         gas: gasEstimate
       };
+
+      console.log('Send transaction:', sendTransaction);
   
       // Send the transaction with the estimated gas
-      const result = await window.ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [sendTransaction]
-      });
+      const result = await walletClient.writeContract(sendTransaction);
   
       console.log(result);
     } catch (error) {
@@ -167,12 +199,10 @@ const transaction = {
   useEffect(() => {
     async function loadWeb3() {
       if (window.ethereum) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const provider = sdk.makeWeb3Provider({ options: 'all' });
         setProvider(provider);
-        const signer = provider.getSigner();
-        setSigner(signer);
         try {
-          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' }) 
           const account = ethers.utils.getAddress(accounts[0]);
           setAccount(account);
           console.log(`Connected with address: ${account}`);
@@ -182,6 +212,13 @@ const transaction = {
       } else {
         console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
       }
+
+      const client = createWalletClient({
+        account, 
+        chain: viemChains[network],
+        transport: custom(window.ethereum)
+      })
+      setSigner(client);
     }
 
     loadWeb3();
@@ -191,19 +228,44 @@ const transaction = {
 
   return (
     <div className="App">
-      <Navigation />
-      <h1>Interact with Ethereum Smart Contract</h1>
-    <label htmlFor="networkSelector">Select Network:</label>
-    <select id="networkSelector" value={network} onChange={handleNetworkChange}>
-        <option value="mainnet">Mainnet</option>
-        <option value="arbitrum">Arbitrum Mainnet</option>
-        <option value="arbitrum_sepolia">Arbitrum Sepolia Testnet</option>
-    </select>
-      <input type="text" id="contractAddress" placeholder="Enter Smart Contract Address" />
+  <Navigation />
+  <h1>Interact with EVM Smart Contract</h1>
+<label htmlFor="networkSelector">Select Network:</label>
+<Select
+  id="networkSelector"
+  defaultValue={options.find(option => option.value === 'mainnet')}
+  options={options}
+  onChange={selectedOption => handleNetworkChange(selectedOption.value)}
+/>
+<input 
+  type="text" 
+  placeholder="Enter Smart Contract Address" 
+  maxLength="42" 
+  minLength="42" 
+  style={{ width: '400px' }}
+  value={address} // Add this line
+  onChange={e => {
+    const address = e.target.value;
+    setAddress(address);
+    setIsAddressEmpty(false);
+    setIsAddressTooShort(address.length < 42);
+  }} 
+/>
+        {isAddressTooShort && <p style={{ color: 'red' }}>Address is too short!</p>}
       <div>
         {/* ... */}
-        <textarea id="contractABI" placeholder="Enter Contract ABI (JSON format)" />
+        <textarea 
+            style={{ width: '500px' }}
+            id="contractABI" placeholder="Enter Contract ABI (JSON format)" />
         <button id="connectButton" onClick={handleConnect}>Connect</button>
+        {isAddressEmpty && <p style={{ color: 'red' }}>Address is required!</p>}
+        <div>
+  {output && Object.entries(output).map(([key, value]) => (
+    <div key={key}>
+      <strong>{key}:</strong> {value}
+    </div>
+  ))}
+</div>
         {abi && abi.map((item, index) => {
   if (item.inputs && item.inputs.length > 0) {
     return (
