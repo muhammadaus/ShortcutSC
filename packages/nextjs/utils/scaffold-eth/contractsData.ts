d1c4a7a856c66b45088cff9320b14264aeb812da
MUHAMMAD AUS <103428274+muhammadaus@users.noreply.github.com>
2024-07-05 20:58:39 +0900

    add types for getAllContracts

--- packages/nextjs/utils/scaffold-eth/contractsData.ts
+++ packages/nextjs/utils/scaffold-eth/contractsData.ts
@@ -1,8 +1,12 @@
 import scaffoldConfig from "~~/scaffold.config";
 import { contracts } from "~~/utils/scaffold-eth/contract";
 
-export function getAllContracts() {
-  let allContractsData = {};
+interface ContractData {
+  external?: boolean;
+}
+
+export function getAllContracts(): Record<string, ContractData> {
+  let allContractsData: Record<string, ContractData> = {};
   for (const network of scaffoldConfig.targetNetworks) {
     const contractsData = contracts?.[network.id];
     if (contractsData) {
